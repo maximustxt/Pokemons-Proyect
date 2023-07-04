0648d470ba962b89aee95444ce2579058fdc7a46
Ignacio Martin <119757609+maximustxt@users.noreply.github.com>
2023-07-04 15:01:11 -0300

    Cambios Proyecto

--- Back-End-Pokemon/src/DB.ts
+++ Back-End-Pokemon/src/DB.ts
@@ -1,49 +1,14 @@
 import { Sequelize } from "sequelize-typescript";
 require("dotenv").config();
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DIALECT, BDD, DB_DEPLOY } =
-  process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, BDD } = process.env;
 
-// Comprueba si DB_DEPLOY está definido y úsalo en consecuencia
-const databaseURL = DB_DEPLOY
-  ? DB_DEPLOY
-  : "postgres://db_deploy_z5j3_user:D2zR2A65ePTesxJKoTTPYw7cjh9OdsPL@dpg-cii2u75gkuvojje9ks40-a.oregon-postgres.render.com/db_deploy_z5j3"; // Puedes proporcionar un valor predeterminado en caso de que DB_DEPLOY sea undefined
-
-// Configura las opciones de conexión a la base de datos
-const dbOptions = {
+export const sequelize = new Sequelize({
   host: DB_HOST,
-  port: 5432, // Convierte el puerto a un número
-  dialect: DB_DIALECT,
+  dialect: "postgres",
   database: BDD,
   username: DB_USER,
   password: DB_PASSWORD,
-};
-
-// Configura las opciones adicionales para Sequelize
-const sequelizeOptions = {
-  dialectOptions: {
-    ssl: {
-      require: true,
-    },
-  },
-  // Define la ruta a los archivos de los modelos
-  models: [__dirname + "/models/*.ts"],
-};
-
-// Crea una instancia de Sequelize y pásale las opciones
-export const sequelize = new Sequelize(databaseURL, sequelizeOptions);
-
-// import { Sequelize } from "sequelize-typescript";
-// require("dotenv").config();
-
-// const { DB_USER, DB_PASSWORD, DB_HOST, BDD, DB_DEPLOY } = process.env;
-
-// export const sequelize = new Sequelize(DB_DEPLOY, {
-//   host: DB_HOST,
-//   dialect: "postgres",
-//   database: BDD,
-//   username: DB_USER,
-//   password: DB_PASSWORD,
-//   port: 5432, // Puerto por defecto de PostgreSQL
-//   models: [__dirname + "/models/*.ts"], // Ruta a los archivos de los modelos (ME FALTABA ESTO , PARA QUE SE BUSQUEN LOS MODELOS Y PÓDER INTERACTUAR CON LOS MISMOS.)
-// });
+  port: 5432, // Puerto por defecto de PostgreSQL
+  models: [__dirname + "/models/*.ts"], // Ruta a los archivos de los modelos (ME FALTABA ESTO , PARA QUE SE BUSQUEN LOS MODELOS Y PÓDER INTERACTUAR CON LOS MISMOS.)
+});
